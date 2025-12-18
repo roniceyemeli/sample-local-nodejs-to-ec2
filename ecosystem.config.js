9d1d1319a2b438a0730d8bfd6fa8f9f734ab3f42
roniceyemeli <roniceyemeli@gmail.com>
2025-12-18 15:03:42 +0100

    add gitignoe file

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: "my-app",
+      script: "app.js",
+      instances: "max",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
