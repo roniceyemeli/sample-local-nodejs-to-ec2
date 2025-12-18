8f5ba9a6d081ee142766450a6d6e5fb8f45dd3d5
roniceyemeli <roniceyemeli@gmail.com>
2025-12-18 15:40:46 +0100

    updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World  i am updated");
 });
 app.listen(port, () => {
   console.log(`Example server updated listening at http://localhost:${port}`);
