bfc0c5b8e1e061865e40214cbb21b3bd314ef1af
roniceyemeli <roniceyemeli@gmail.com>
2025-12-18 15:47:05 +0100

    updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,9 @@ module.exports = {
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "development",
-        PORT: 3000,
       },
       env_production: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
     },
   ],
